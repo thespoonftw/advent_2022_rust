3e9ab8484b662487a501d5312d9965b377980e87
Michael Wright <michael@cronai.ai>
2022-12-04 19:25:24 +0000

    day 4

--- src/day04.rs
+++ src/day04.rs
@@ -5,15 +5,24 @@ pub struct Day04();
 impl Problem for Day04 {
 
     fn part_one(&self, input: &str) -> String {
-        return input.len().to_string();
+        return input.lines().filter(|l| is_contained(l)).count().to_string();
     }
 
     fn part_two(&self, input: &str) -> String {
-        return input.len().to_string();
+        return input.lines().filter(|l| is_overlap(l)).count().to_string();
     }
 }
 
-impl Day04 {
+fn is_contained(line: &str) -> bool {
+    let s = split_line(line);
+    return (s[0] >= s[2] && s[1] <= s[3]) || (s[2] >= s[0] && s[3] <= s[1]);
+}
 
-    
+fn is_overlap(line: &str) -> bool {
+    let s = split_line(line);
+    return !(s[0] > s[3] || s[1] < s[2]);
+}
+
+fn split_line(line: &str) -> Vec<i32> {
+    return line.split([',', '-']).map(|c| c.parse().unwrap()).collect();
 }
