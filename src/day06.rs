e1673ec36e4d888faa1203e74ba1cf707c9d17fd
Michael Wright <michael@cronai.ai>
2022-12-06 12:29:51 +0000

    day 6

--- src/day06.rs
+++ src/day06.rs
@@ -1,19 +1,30 @@
 use crate::problem::Problem;
+use std::collections::HashSet;
 
 pub struct Day06();
 
 impl Problem for Day06 {
 
     fn part_one(&self, input: &str) -> String {
-        return input.len().to_string();
+        return find_key(input, 4)
     }
 
     fn part_two(&self, input: &str) -> String {
-        return input.len().to_string();
+        return find_key(input, 14)
     }
 }
 
-impl Day06 {
+fn find_key(input: &str, length: usize) -> String {
 
-    
+    let chars = input.chars().collect::<Vec<char>>();
+
+    for i in length..chars.len() {
+        let set:HashSet<char> = chars[i-length..i].to_vec().into_iter().collect();
+        if set.len() == length {
+            return i.to_string();
+        }
+    }
+
+    return "".to_string();
+
 }
